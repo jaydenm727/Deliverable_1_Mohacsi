735fb8b4d5d487250b1ce1ba425d8e1df60c37eb
Jayden <Jayden@DESKTOP-7EL9AMF>
2023-01-15 15:16:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Deliverable_1_Mohacsi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string mystring = "Hello, World!";
+            Console.WriteLine("Solution (2): " + mystring);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter the user's age: ");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + userAge);
+        }
+    }
+}
